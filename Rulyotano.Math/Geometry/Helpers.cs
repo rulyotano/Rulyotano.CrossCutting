b68e8e5c1bf36baaeb41acef1f531fc426bf73db
Raúl Otaño Hurtado <rulyotano@gmail.com>
2021-01-19 01:00:29 +0100

    First commit. Added first version.

---
+++ Rulyotano.Math/Geometry/Helpers.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rulyotano.Math.Geometry
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Convert from radians to degree
+        /// </summary>
+        /// <param name="radian"></param>
+        /// <returns></returns>
+        public static float RadianToDegreeConvert(float radian)
+        {
+            //return (180*radian)/Math.PI;
+            return 57.29577951308232f * radian;
+        }
+
+        /// <summary>
+        /// Convert degree to radians
+        /// </summary>
+        /// <param name="degree"></param>
+        /// <returns></returns>
+        public static double DegreeToRadianConvert(double degree)
+        {
+            return (degree * System.Math.PI) / 180;
+        }
+
+        /// <summary>
+        /// Euclidean distance. From points.
+        /// </summary>
+        /// <param name="point1"></param>
+        /// <param name="point2"></param>
+        /// <returns></returns>
+        public static double EuclideanDistance(Point point1, Point point2)
+        {
+            var xDiff = point2.X - point1.X;
+            var yDiff = point2.Y - point1.Y;
+            return System.Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+
+        /// <summary>
+        /// Euclidean distance. From points coordinates.
+        /// </summary>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <returns></returns>
+        public static double EuclideanDistance(double x1, double y1, double x2, double y2)
+        {
+            var xDiff = x2 - x1;
+            var yDiff = y2 - y1;
+            return System.Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+
+        /// <summary>
+        /// Find best place to insert a new point by minimizing the total length.
+        /// Useful for instance when want to add points to an ordered points sequence,
+        /// that could be interpolated then using Beizer curves.
+        /// </summary>
+        /// <param name="newPoint"></param>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        public static int BestPlaceToInsert(Point newPoint, List<Point> points)
+        {
+            if (points.Count == 0) return 0;
+            if (points.Count == 1) return 1;
+
+            var bestIndex = 0;
+            var bestDistance = EuclideanDistance(newPoint, points.First());
+
+            for (int i = 1; i < points.Count; i++)
+            {
+                var previousPoint = points[i - 1];
+                var currentPoint = points[i];
+                var oldDistance = EuclideanDistance(previousPoint, currentPoint);
+                var distance1 = EuclideanDistance(previousPoint, newPoint);
+                var distance2 = EuclideanDistance(newPoint, currentPoint);
+                var distanceDifference = distance1 + distance2 - oldDistance;
+                if (distanceDifference < bestDistance)
+                {
+                    bestDistance = distanceDifference;
+                    bestIndex = i;
+                }
+            }
+
+
+            var lastDistance = EuclideanDistance(points.Last(), newPoint);
+
+            if (lastDistance < bestDistance)
+            {
+                bestIndex = points.Count;
+            }
+
+            return bestIndex;
+        }
+    }
+}
---
+++ Rulyotano.Math/Numeric.cs
@@ -0,0 +1,46 @@
+﻿namespace Rulyotano.Math
+{
+    public static class Numeric
+    {
+        public const double Epsilon = 0.00001;
+
+        /// <summary>
+        /// Equal function between two doubles
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static bool DoubleEquals(double d1, double d2, double error = Epsilon)
+        {
+            if (double.IsNegativeInfinity(d1) && double.IsNegativeInfinity(d2) || double.IsPositiveInfinity(d1) && double.IsPositiveInfinity(d2) || double.IsNaN(d1) && double.IsNaN(d2))
+                return true;
+            return System.Math.Abs(d1 - d2) < error;
+        }
+
+        /// <summary>
+        /// Equal function between two float values
+        /// </summary>
+        /// <param name="f1"></param>
+        /// <param name="f2"></param>
+        /// <param name="error"></param>
+        /// <returns></returns>
+        public static bool FloatEquals(float f1, float f2, double error = Epsilon)
+        {
+            if (float.IsNegativeInfinity(f1) && float.IsNegativeInfinity(f2) || float.IsPositiveInfinity(f1) && float.IsPositiveInfinity(f2) || float.IsNaN(f1) && float.IsNaN(f2))
+                return true;
+            return System.Math.Abs(f1 - f2) < error;
+        }
+
+        /// <summary>
+        /// Middle distance between two values
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static double Middle(double value1, double value2)
+        {
+            return (value1 + value2) / 2.0;
+        }
+    }
+}

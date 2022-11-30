24db2e577170ac701923c45d8d26ceecadabd9d8
Raul <rulyotano@gmail.com>
2022-11-30 13:19:00 +0100

    Implemented CIF number for Spain documents

---
+++ tests/Rulyotano.I18N.IdentityNumbers.Spain.Tests/DocumentsTests/CifTests/CifWhenCheckingIsValid.cs
@@ -0,0 +1,83 @@
+﻿using Rulyotano.I18N.IdentityNumbers.Spain.Documents;
+using Xunit;
+
+namespace Rulyotano.I18N.IdentityNumbers.Spain.Tests.DocumentsTests.CifTests
+{
+    public class CifWhenCheckingIsValid
+    {
+        private readonly Cif _cifDocument;
+
+        public CifWhenCheckingIsValid()
+        {
+            _cifDocument = new Cif();
+        }
+
+        [Theory]
+        [InlineData("A50923085")]
+        [InlineData("B01073253")]
+        [InlineData("C20063137")]
+        [InlineData("D16712341")]
+        [InlineData("E50130947")]
+        [InlineData("F48581664")]
+        [InlineData("G26510321")]
+        [InlineData("H75698704")]
+        [InlineData("J40550634")]
+        [InlineData("N0099964I")]
+        [InlineData("P3342972A")]
+        [InlineData("Q0065830B")]
+        [InlineData("R0048766J")]
+        [InlineData("S1046245E")]
+        [InlineData("U13426770")]
+        [InlineData("V22356950")]
+        [InlineData("W2315753J")]
+        public void Should_ValidateCorrectDocuments(string documentNumber)
+        {
+            Assert.True(_cifDocument.IsValid(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("A50922085")]
+        [InlineData("B01074253")]
+        [InlineData("C20061137")]
+        [InlineData("D16712541")]
+        [InlineData("N0199964I")]
+        [InlineData("P3442972A")]
+        public void Should_NotValidate_When_ChecksumIsWrong(string documentNumber)
+        {
+            Assert.False(_cifDocument.IsValid(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("A5092308Z")]
+        [InlineData("B0107325Q")]
+        [InlineData("C2006313K")]
+        public void Should_NotValidate_When_NotSupportedChecksumCharacter(string documentNumber)
+        {
+            Assert.False(_cifDocument.IsValid(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("A5092208225")]
+        [InlineData("B010334474253")]
+        [InlineData("C201137")]
+        public void Should_NotValidate_When_NumberLengthIsNotCorrect(string documentNumber)
+        {
+            Assert.False(_cifDocument.IsValid(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("a50923085")]
+        [InlineData("b01073253")]
+        [InlineData("w2315753j")]
+        public void Should_ValidateWhenLowercase(string documentNumber)
+        {
+            Assert.True(_cifDocument.IsValid(documentNumber));
+        }
+
+        [Fact]
+        public void Should_NotValidate_Null()
+        {
+            Assert.False(_cifDocument.IsValid(null));
+        }
+    }
+}
---
+++ tests/Rulyotano.I18N.IdentityNumbers.Spain.Tests/DocumentsTests/CifTests/CifWhenCheckingTypeOfDocument.cs
@@ -0,0 +1,70 @@
+﻿using Rulyotano.I18N.IdentityNumbers.Spain.Documents;
+using Xunit;
+
+namespace Rulyotano.I18N.IdentityNumbers.Spain.Tests.DocumentsTests.CifTests
+{
+    public class CifWhenCheckingTypeOfDocument
+    {
+        private readonly Cif _cifDocument;
+
+        public CifWhenCheckingTypeOfDocument()
+        {
+            _cifDocument = new Cif();
+        }
+
+        [Theory]
+        [InlineData("N4932226F")]
+        [InlineData("G00617787")]
+        [InlineData("C01287572")]
+        [InlineData("Q1865289A")]
+        [InlineData("P5203822A")]
+        [InlineData("S7346204F")]
+        public void Should_ValidateCorrectDocuments(string documentNumber)
+        {
+            Assert.True(_cifDocument.IsOfType(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("N4932226K")]
+        [InlineData("G0061778L")]
+        [InlineData("C0128757Z")]
+        public void Should_NotValidate_When_NumberEndsInNotSupportedCharacter(string documentNumber)
+        {
+            Assert.False(_cifDocument.IsOfType(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("14333663V")]
+        [InlineData("Z2463117Y")]
+        [InlineData("Y6647030K")]
+        [InlineData("60080190G")]
+        public void Should_NotValidate_When_OtherTypeOfDocuments(string documentNumber)
+        {
+            Assert.False(_cifDocument.IsOfType(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("N49322263F")]
+        [InlineData("G0061577827")]
+        [InlineData("C012875")]
+        public void Should_NotValidate_When_NumberLengthIsNotCorrect(string documentNumber)
+        {
+            Assert.False(_cifDocument.IsOfType(documentNumber));
+        }
+
+        [Theory]
+        [InlineData("n4932226f")]
+        [InlineData("g00617787")]
+        [InlineData("c01287572")]
+        public void Should_ValidateWhenLowercase(string documentNumber)
+        {
+            Assert.True(_cifDocument.IsOfType(documentNumber));
+        }
+
+        [Fact]
+        public void Should_NotValidate_Null()
+        {
+            Assert.False(_cifDocument.IsOfType(null));
+        }
+    }
+}

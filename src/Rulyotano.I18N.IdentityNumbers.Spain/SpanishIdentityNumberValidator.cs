1c80666e5aaca8f74dd05e28a09a0aa76196fee4
Raul <rulyotano@gmail.com>
2022-12-01 12:27:12 +0100

    Update Spanish id number library. Improve documentation. (#14)

--- src/Rulyotano.I18N.IdentityNumbers.Spain/SpanishIdentityNumberValidator.cs
+++ src/Rulyotano.I18N.IdentityNumbers.Spain/SpanishIdentityNumberValidator.cs
@@ -1,16 +1,23 @@
 ﻿using Rulyotano.I18N.IdentityNumbers.Spain.Documents;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Rulyotano.I18N.IdentityNumbers.Spain
 {
     public class SpanishIdentityNumberValidator : IIdentityNumberValidator
     {
+        private const string CountryRegex = @"^(ESP|ES|[A-Z]{2}-ES)$";
         private readonly IEnumerable<ISpanishDocument> _documents;
 
         public SpanishIdentityNumberValidator()
         {
             _documents = new ISpanishDocument[] { new Dni(), new Nie(), new Cif() };
+        }
+
+        public bool IsCountry(string countryCode)
+        {
+            return new Regex(CountryRegex).IsMatch(countryCode?.ToUpperInvariant() ?? string.Empty);
         }
 
         public bool IsValid(string identityNumber)

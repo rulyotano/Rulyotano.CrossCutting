fec55dcf907f5fc145462f14b449966901ac57b9
Raul <rulyotano@gmail.com>
2024-09-24 14:49:47 +0200

    Implement search tests with no lps

--- src/Rulyotano.Algorithms/Strings/KmpSearch.cs
+++ src/Rulyotano.Algorithms/Strings/KmpSearch.cs
@@ -4,8 +4,34 @@ using System.Text;
 
 namespace Rulyotano.Algorithms.Strings
 {
-    public class KmpSearch
+    public static class KmpSearch
     {
+        private const int NotFoundValue = -1;
 
+        public static int SearchFirstOccurence(this string text, string s)
+        {
+            if (string.IsNullOrEmpty(text) 
+                || string.IsNullOrEmpty(s)
+                || text.Length < s.Length)
+                return NotFoundValue;
+
+            var i = 0;
+            while (i < text.Length)
+            {
+                var si = 0;
+                while (si < s.Length)
+                {
+                    if (i + si >= text.Length || text[i + si] != s[si])
+                        break;
+                    si++;
+                }
+
+                if (si == s.Length)
+                    return i;
+                i++;
+            }
+
+            return NotFoundValue;
+        }
     }
 }
---
+++ tests/Rulyotano.Algorithms.Tests/StringTests/KmpSearchFirstOccurenceTests.cs
@@ -0,0 +1,63 @@
+﻿using Rulyotano.Algorithms.Strings;
+using Xunit;
+
+namespace Rulyotano.Algorithms.Tests.StringTests
+{
+    public class KmpSearchTests
+    {
+        private const int NotFound = -1;
+
+        [Theory]
+        [InlineData("", "abc")]
+        [InlineData("abc", "")]
+        [InlineData(null, "abc")]
+        [InlineData("abc", null)]
+        public void WhenNullOrEmptyShouldReturnNotFound(string text, string s)
+        {
+            var result = text.SearchFirstOccurence(s);
+            Assert.Equal(NotFound, result);
+        }
+        
+        [Fact]
+        public void WhenTextLengthIsLowerThanSearchLengthShouldReturnNotFound()
+        {
+            var result = "ab".SearchFirstOccurence("abc");
+            Assert.Equal(NotFound, result);
+        }
+
+        [Fact]
+        public void WhenEqualStringsShouldReturnZero()
+        {
+            var result = "abc".SearchFirstOccurence("abc");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void WhenAreNotEqualsShouldReturnNotFound()
+        {
+            var result = "abd".SearchFirstOccurence("abc");
+            Assert.Equal(NotFound, result);
+        }
+
+        [Fact]
+        public void WhenFirstOccurenceIsNotStartingOfStringShouldReturnCorrectIndex()
+        {
+            var result = "aaaabcdefg".SearchFirstOccurence("abc");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void WhenCorrenceIsAtTheEndOfTheStringShouldReturnCorrectIndex()
+        {
+            var result = "abeateabc".SearchFirstOccurence("abc");
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void WhenThereIsNoOccurenceShouldReturnNotFound()
+        {
+            var result = "aaab".SearchFirstOccurence("abc");
+            Assert.Equal(-1, result);
+        }
+    }
+}
